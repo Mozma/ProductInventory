0cd9cb4f9bf76d1f362fc7b64531d07ec4084576
Ilia Zadornov <il.mozma@gmail.com>
2020-01-24 21:33:33 +0300

    ProductInventory

---
+++ Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace ProductInventory
+{
+    class Product
+    {
+        public static Random rnd = new Random();
+
+        public int Amount { get; set; }
+        public double Price { get; set; }
+        public int Number { get; set; }
+
+
+        public Product()
+        {
+            Amount = rnd.Next(100);
+            Price = rnd.Next(10, 900);
+            Number = rnd.Next(10000);
+        }
+
+        public Product(int amount, double price)
+        {
+            Amount = amount;
+            Price = price;
+            Number = rnd.Next(10000);
+        }
+
+        public double getTotalCost()
+        {
+            return Price * Amount;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+
+namespace ProductInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Inventory inventory = new Inventory();
+
+            welcomCommand();
+           
+            var tmp = Console.ReadLine();
+            while (tmp != "q")
+            {
+                bool f1 = true;
+                switch (tmp.ToLower())
+                {
+                    case "show":
+                        inventory.printProductList();
+                        break;
+
+                    case "help":
+                        printCommand();
+                        break;
+
+                    case "gen":
+                        genCommand(inventory);
+                        break;
+
+                    default:
+                        Console.WriteLine("Command not found. Type \"help\" for more info.");
+                        break;
+
+                }
+
+                tmp = Console.ReadLine();
+            }
+
+
+        }
+
+
+
+
+
+        public static void genCommand(Inventory inv)
+        {
+            Console.WriteLine("How much?");
+
+            int length = 0;
+            while (!Int32.TryParse(Console.ReadLine(), out length))
+            {
+                Console.Write("That's not a number! Try again: ");
+            }
+            Console.WriteLine($"Genegated {length} products.");
+
+            for (var i = 0; i < length; i++)
+            {
+                inv.addProduct(new Product());
+            }
+        }
+
+        public static void welcomCommand()
+        {
+            Console.WriteLine(new string('-', 30));
+            Console.WriteLine("        ProductInventory");
+            Console.WriteLine(new string('-', 30));
+            printCommand();
+        }
+        
+        public static void printCommand()
+        {
+            Console.WriteLine("{0,10} - Print all commands.", "help");
+            Console.WriteLine("{0,10} - Show Inventory list.", "show");
+            Console.WriteLine("{0,10} - Generate some products.", "gen");
+            Console.WriteLine("{0,10} - Clear screen.", "clear");
+        }
+
+
+
+
+
+
+
+    }
+}
